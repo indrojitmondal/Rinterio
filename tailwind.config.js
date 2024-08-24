0ceefc35c6004e609e58c88c541142948068cd66
indroji-mondal <indro.cse.bu@gmail.com>
2024-08-24 23:41:13 +0600

    updated send mail arrow icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,26 @@ module.exports = {
       extend: {},
     },
     plugins: [],
+
+    colors: {
+                   
+      secondary: 'rgb(17, 17, 17)',
+      body: 'rgb(248, 246, 243)',
+      accent: 'rgb(106, 151, 57)',
+      body_neutral: 'rgb(51, 51, 51)',
+      primary: 'rgb(171, 239, 95)',
+      header_bg: '#F7F7F7',
+      about: '#F4F4F4',
+      secondary: 'rgba(171, 239, 95, 0.5)',
+      text_color:'rgb(73, 73, 73)',
+      h_color: 'rgb(33, 33, 33)',
+      portfolio: 'rgb(255, 255, 255)',
+      border_c:'rgb(233, 233, 233)',
+      clg_p:'rgb(115,115, 115)',
+      connect:'rgba(171, 239, 95, 0.2)',
+      footer_bg:'rgb(28, 28, 28)',
+
+  },
+
+
   }
